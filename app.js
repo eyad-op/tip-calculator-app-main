795bee4f6297b267643c3336498aee70931521d6
eyad-op <eyad.khalil.91@gmail.com>
2021-07-19 15:05:55 +0300

    commit

--- app.js
+++ app.js
@@ -2,10 +2,3 @@
 // tipVal class
 // active class
 //tipButtons
-
-let tipButtons = document.getElementById("tipButtons");
-console.log(tipButtons);
-function val() {
-  var value = Number(document.getElementsByClassName("tipVal").innerHTML);
-  console.log(value);
-}
