f1b5e3e418fb5dcf7054ec0b2c1180332f625172
eyad-op <eyad.khalil.91@gmail.com>
2021-07-19 14:48:03 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// bill id
+// tipVal class
+// active class
+//tipButtons
+
+let tipButtons = document.getElementById("tipButtons");
+console.log(tipButtons);
+function val() {
+  var value = Number(document.getElementsByClassName("tipVal").innerHTML);
+  console.log(value);
+}
